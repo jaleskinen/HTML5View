38bed70c9b7a07cc597a054b1ef2cfbcbf6442c4
jaleskinen <jarmo.leskinen@hepokumpu.com>
2015-10-30 10:41:21 +0200

    30.10.2015 Jarmo Leskinen. Updated

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -7,15 +7,16 @@ $(document).ready(function () {
     console.log("jquery onload triggered");
     //$("nav").css("background-color","lightblue");
     //$("footer").css("background-color", "darkblue");
-    $("[class]").css("background-color", "grey");
+    //$("[class]").css("background-color", "grey");
     $("#head").css("background-color", "red").css("padding", "20px").css("border-radius", "8px");
     
     $(".about").html("New text from jQuery"); //Add new text.
     //$("[data-dummy]").html("<p>Hello World</p>"); //Muuttaa data-dummy elementin tekstin
     
+    
     var setting =  {
         
-        method: "GET",
+        method: "GET",  //default method is GET
         url: "http://localhost:28017/oma/person/",
         dataType: "jsonp",
         jsonp: "jsonp"
@@ -25,15 +26,43 @@ $(document).ready(function () {
     
     $.ajax(setting).done(function(data) {
         console.log(data);
+  
+        //get all keys (attribute names)from json object
+        console.log(Object.keys(data.rows[0]));
+        
+        //Create headers also dynamically, check that rows length is > 0
+        if(data.rows.length > 0) {
+            var headers = Object.keys(data.rows[0]);
+            //Create row for headers
+            var row = $("<tr></tr>");
+            for (i = 1; i < headers.length; i++) {
+                //Create header and add it to row
+                $("<th>" + headers[i] + "</th>").appendTo(row);
+                }   
+                //Add row to thead element
+                $(row).appendTo("thead");
+        }
+
+        
         for (i = 0; i < data.rows.length; i++) {
+            
+            
+            /*html += "<tr>";
+            for(var k =0;k<headers.length;k++)
+                {
+                    html += "<td>" + data.rows[k][headers[k]] + "</td>;"
+                }
+            html += "</tr>"; */
             
             var html = "<tr>" +
                         "<td>" + data.rows[i].name + "</td>" +
                         "<td>" + data.rows[i].address + "</td>" +
                         "<td>" + data.rows[i].age + "</td>" +
+                        "<td>" + data.rows[i].email + "</td>" +
                         "</tr>";
             
-            $(html).appendTo("tbody"); 
+            $(html).appendTo("tbody"); //All tbody tables, use selectors to use only one if several tbodys in use.
+            
             
         }
     });
