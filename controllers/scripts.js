a538962638f8603c167c449a301e08cf40e52b45
jaleskinen <jarmo.leskinen@hepokumpu.com>
2015-10-31 18:00:13 +0200

    31.10.2015 Jarmo Leskinen. JSLint warnings cleaned/removed

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,18 +1,19 @@
-//"use strict";
+/*JSLint warning settings*/
+/*jslint  plusplus: true, devel: true*/
+/*global $ */
+
 console.log("Here we go again!");
 
 //Wait document ready event
 $(document).ready(function () {
-    
+    "use strict";
     console.log("jquery onload triggered");
     //$("nav").css("background-color","lightblue");
     //$("footer").css("background-color", "darkblue");
     //$("[class]").css("background-color", "grey");
-    $("#head").css("background-color", "red").css("padding", "20px").css("border-radius", "8px");
-    
-    $(".about").html("New text from jQuery"); //Add new text.
+    //$("#head").css("background-color", "red").css("padding", "20px").css("border-radius", "8px");
+    //$(".about").html("New text from jQuery"); //Add new text.
     //$("[data-dummy]").html("<p>Hello World</p>"); //Muuttaa data-dummy elementin tekstin
-    
     
     var setting =  {
         
@@ -25,23 +26,24 @@ $(document).ready(function () {
     };
     
     
-    $.ajax(setting).done(function(data) {
+    $.ajax(setting).done(function (data) {
         console.log(data);
+        var i = 0, k = 0, headers = 0, row = 0, row2 = 0;
   
         //get all keys (attribute names)from json object
         console.log(Object.keys(data.rows[0]));
         
         //Create headers also dynamically, check that rows length is > 0
-        if(data.rows.length > 0) {
-            var headers = Object.keys(data.rows[0]);
+        if (data.rows.length > 0) {
+            headers = Object.keys(data.rows[0]);
             //Create row for headers
-            var row = $("<tr></tr>");
+            row = $("<tr></tr>");
             for (i = 1; i < headers.length; i++) {
                 //Create header and add it to row
                 $("<th>" + headers[i] + "</th>").appendTo(row);
-                }
-                //Add row to thead element
-                $(row).appendTo("thead");
+            }
+            //Add row to thead element
+            $(row).appendTo("thead");
         }
 
         
@@ -59,17 +61,17 @@ $(document).ready(function () {
             //All tbody tables, use selectors to use only one if several tbodys in use.
             
             //Create data rows also dynamically, check that rows length is > 0
-            if(data.rows.length > 0) {
+            if (data.rows.length > 0) {
 
                 //Create row for data
-                var row2 = $("<tr></tr>");
+                row2 = $("<tr></tr>");
                 for (k = 1; k < headers.length; k++) {
-                //Create data and add it to row
-                $("<td>" + data.rows[i][headers[k]] + "</td>").appendTo(row2);
+                    //Create data and add it to row
+                    $("<td>" + data.rows[i][headers[k]] + "</td>").appendTo(row2);
                 }
                 //Add row to thead element
                 $(row2).appendTo("tbody");
-            }    
+            }
         }
     });
         
